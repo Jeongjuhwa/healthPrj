7860398ed687d3f618fc54d03e2fac46b2254fa9
82109 <qwe199942@gmail.com>
2021-09-04 16:52:55 +0900

    Initial commit

---
+++ src/main/java/healthApp/healthPrj/HealthPrjApplication.java
@@ -0,0 +1,13 @@
+package healthApp.healthPrj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthPrjApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthPrjApplication.class, args);
+	}
+
+}
---
+++ src/test/java/healthApp/healthPrj/HealthPrjApplicationTests.java
@@ -0,0 +1,13 @@
+package healthApp.healthPrj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthPrjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
