1cd2a8b30ae35ec476c5b6875ba1a7ab9732bbc0
82109 <qwe199942@gmail.com>
2021-09-05 21:28:38 +0900

    Member registration implementation

--- src/main/java/healthApp/healthPrj/entity/Address.java
+++ src/main/java/healthApp/healthPrj/entity/Address.java
@@ -14,4 +14,10 @@ public class Address {
     private String city;
     private String street;
     private String zipcode;
+
+    public Address(String city, String street, String zipcode) {
+        this.city = city;
+        this.street = street;
+        this.zipcode = zipcode;
+    }
 }
--- src/main/java/healthApp/healthPrj/entity/base/BaseTimeEntity.java
+++ src/main/java/healthApp/healthPrj/entity/base/BaseTimeEntity.java
@@ -4,9 +4,8 @@ import lombok.Getter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
+
+import javax.persistence.*;
 import java.time.LocalDateTime;
 
 @EntityListeners(AuditingEntityListener.class)
@@ -20,6 +19,19 @@ public class BaseTimeEntity {
 
     @LastModifiedDate
     private LocalDateTime lastModifiedDate;
+
+    @PrePersist
+    public void prePersist(){
+        LocalDateTime now = LocalDateTime.now();
+        createdDate = now;
+        lastModifiedDate = now;
+    }
+
+    @PreUpdate
+    public void preUpdate(){
+        lastModifiedDate = LocalDateTime.now();
+    }
+
 
 
 }
