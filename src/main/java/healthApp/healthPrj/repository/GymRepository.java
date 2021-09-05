f90bfcc69e38584377078e1333a2662b865ee546
82109 <qwe199942@gmail.com>
2021-09-05 19:48:24 +0900

    add Query

--- src/main/java/healthApp/healthPrj/repository/GymRepository.java
+++ src/main/java/healthApp/healthPrj/repository/GymRepository.java
@@ -3,5 +3,9 @@ package healthApp.healthPrj.repository;
 import healthApp.healthPrj.entity.Gym;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface GymRepository extends JpaRepository<Gym, Long> {
+
+    Optional<Gym> findByGymNumber(String gymNumber);
 }
