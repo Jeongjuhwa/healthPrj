bd5e217ce7cbd08e463914aa0740ad6d20ac4030
82109 <qwe199942@gmail.com>
2021-09-05 20:42:27 +0900

    create controller

---
+++ src/main/java/healthApp/healthPrj/controller/MemberApiController.java
@@ -0,0 +1,9 @@
+package healthApp.healthPrj.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberApiController {
+}
