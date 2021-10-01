37f45964c91382909bd8fc08003bb27702b8fe94
82109 <qwe199942@gmail.com>
2021-10-02 03:02:35 +0900

    commit: MemberSearch 동적쿼리 추가

--- src/main/java/healthApp/healthPrj/domain/member/repository/MemberRepositoryImpl.java
+++ src/main/java/healthApp/healthPrj/domain/member/repository/MemberRepositoryImpl.java
@@ -1,11 +1,53 @@
 package healthApp.healthPrj.domain.member.repository;
 
+import com.querydsl.core.types.Projections;
+import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import healthApp.healthPrj.domain.member.dto.MemberDto;
+import healthApp.healthPrj.domain.member.dto.MemberSearch;
+import healthApp.healthPrj.domain.member.model.QMember;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 
 import javax.persistence.EntityManager;
+
+import java.util.List;
+
+import static healthApp.healthPrj.domain.member.model.QMember.member;
 
 @RequiredArgsConstructor
 public class MemberRepositoryImpl implements MemberRepositoryCustom{
 
-    private final EntityManager em;
+    private final JPAQueryFactory query;
+
+    @Override
+    public Page<MemberDto> findByMemberSearch(MemberSearch memberSearch, Pageable pageable) {
+        List<MemberDto> response = query.select(Projections.constructor(MemberDto.class,
+                member.memberName,
+                member.memberAge,
+                member.memberSex,
+                member.address.city,
+                member.address.street,
+                member.address.zipcode
+                )
+        )
+                .from(member)
+                .where(isCondition(memberSearch))
+                .fetch();
+
+        return new PageImpl<>(response, pageable, response.size());
+
+
+    }
+
+    private BooleanExpression isCondition(MemberSearch memberSearch) {
+
+        if(memberSearch.getName() == null){
+            return null;
+        }
+
+        return member.memberName.eq(memberSearch.getName());
+    }
 }
