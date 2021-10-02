298db99a0c69da7ce5f602d3a274b4394b9f17ce
82109 <qwe199942@gmail.com>
2021-10-03 01:59:14 +0900

    commit : InitDB 수정

--- src/main/java/healthApp/healthPrj/InitDb.java
+++ src/main/java/healthApp/healthPrj/InitDb.java
@@ -1,5 +1,6 @@
 package healthApp.healthPrj;
 
+import healthApp.healthPrj.common.enums.JoinStatus;
 import healthApp.healthPrj.common.object.Address;
 import healthApp.healthPrj.domain.gym.model.Gym;
 import healthApp.healthPrj.domain.member.model.Member;
@@ -19,37 +20,122 @@ public class InitDb {
     @PostConstruct
     public void init(){
         initService.dbInit1();
+        initService.dbInit2();
     }
 
 
 
     @Component
-    @Transactional
     @RequiredArgsConstructor
     static class InitService{
 
         public final EntityManager em;
-
+        @Transactional
         public void dbInit1(){
 
-            Gym gym1 = new Gym("spoAny","2233344444",new Address("서울시","공항대로34","12115"));
-            Gym gym2 = new Gym("헬스장","1122233333",new Address("서울시","공항대로376","53215"));
-            Gym gym3 = new Gym("주화헬스","7788899999",new Address("서울시","공항대로285","21833"));
+            Gym gym1 = Gym.builder()
+                    .gymName("spoAny")
+                    .gymNumber("2233344444")
+                    .address(new Address("서울시","공항대로34","12115"))
+                    .build();
+            Gym gym2 = Gym.builder()
+                    .gymName("헬스장")
+                    .gymNumber("1122233333")
+                    .address(new Address("서울시","공항대로376","53215"))
+                    .build();
+            Gym gym3 = Gym.builder()
+                    .gymName("주화헬스")
+                    .gymNumber("7788899999")
+                    .address(new Address("서울시","공항대로285","21833"))
+                    .build();
             em.persist(gym1);
             em.persist(gym2);
             em.persist(gym3);
 
-            Member member1 = new Member("qwe199942@naver.com","963.","정주화",26,"남",new Address("서울시","화곡로350","07953"));
-            Member member2 = new Member("yuntae@naver.com","123.","박윤태",27,"남",new Address("부산시","수영로234","12312"));
-            Member member3 = new Member("gwangtae@naver.com","234.","이태광",27,"남",new Address("부산시","수영로345","43654"));
-            Member member4 = new Member("ohno@naver.com","555.","조현호",27,"남",new Address("부산시","수영로456","51236"));
-            Member member5 = new Member("kojeong@naver.com","666.","고정민",27,"남",new Address("부산시","수영로567","86435"));
-            Member member6 = new Member("moi@naver.com","2134234.","박뫼름",27,"여",new Address("홍천시","시골로111","12565"));
-            Member member7 = new Member("jeyoung@naver.com","34652.","윤제영",27,"여",new Address("부산시","수영로333","12368"));
-            Member member8 = new Member("hyukjin@naver.com","32415235.","손혁진",27,"남",new Address("부산시","수영로666","54543"));
-            Member member9 = new Member("kyeongja@naver.com","32454325.","이경준",27,"남",new Address("부산시","수영로777","23475"));
-            Member member10 = new Member("jjongwu@naver.com","32454325.","김종우",27,"남",new Address("부산시","수영로888","43257"));
 
+        }
+        @Transactional
+        public void dbInit2(){
+            Member member1 = Member.builder()
+                    .emailId("qwe199942@naver.com")
+                    .password("963.")
+                    .memberName("정주화")
+                    .memberAge(26)
+                    .memberSex("남")
+                    .address(new Address("서울시","화곡로350","07953"))
+                    .build();
+            Member member2 = Member.builder()
+                    .emailId("gwangtae@naver.com")
+                    .password("234.")
+                    .memberName("이태광")
+                    .memberAge(27)
+                    .memberSex("남")
+                    .address(new Address("부산시","수영로345","43654"))
+                    .build();
+            Member member3 = Member.builder()
+                    .emailId("ohno@naver.com")
+                    .password("dkssud")
+                    .memberName("조현호")
+                    .memberAge(27)
+                    .memberSex("남")
+                    .address(new Address("부산시","수영로350","12345"))
+                    .build();
+            Member member4 = Member.builder()
+                    .emailId("kojeong@naver.com")
+                    .password("koko")
+                    .memberName("고정민")
+                    .memberAge(27)
+                    .memberSex("남")
+                    .address(new Address("부산시","남천동로12","44003"))
+                    .build();
+            Member member5 = Member.builder()
+                    .emailId("yuntae@naver.com")
+                    .password("tkfkd")
+                    .memberName("박윤태")
+                    .memberAge(27)
+                    .memberSex("남")
+                    .address(new Address("부산시","남천동로235","34503"))
+                    .build();
+            Member member6 = Member.builder()
+                    .emailId("moi@naver.com")
+                    .password("moi77")
+                    .memberName("박뫼름")
+                    .memberAge(27)
+                    .memberSex("여")
+                    .address(new Address("홍천시","시골로111","12565"))
+                    .build();
+            Member member7 = Member.builder()
+                    .emailId("jeyoung@naver.com")
+                    .password("alrnr123")
+                    .memberName("윤제영")
+                    .memberAge(27)
+                    .memberSex("여")
+                    .address(new Address("부산시","수영로257","64783"))
+                    .build();
+            Member member8 = Member.builder()
+                    .emailId("hyukjin@naver.com")
+                    .password("okgolife")
+                    .memberName("손혁진")
+                    .memberAge(27)
+                    .memberSex("남")
+                    .address(new Address("부산시","남천동로21","43211"))
+                    .build();
+            Member member9 = Member.builder()
+                    .emailId("jjongwu@naver.com")
+                    .password("busanuniv")
+                    .memberName("김종우")
+                    .memberAge(27)
+                    .memberSex("남")
+                    .address(new Address("부산시","남천동로26","34112"))
+                    .build();
+            Member member10 = Member.builder()
+                    .emailId("qkyeongja@naver.com")
+                    .password("taekwon")
+                    .memberName("이경준")
+                    .memberAge(27)
+                    .memberSex("남")
+                    .address(new Address("부산시","수영로876","96734"))
+                    .build();
 
             em.persist(member1);
             em.persist(member2);
@@ -62,16 +148,6 @@ public class InitDb {
             em.persist(member9);
             em.persist(member10);
 
-            member1.setGym(gym1);
-            member2.setGym(gym2);
-            member3.setGym(gym3);
-            member4.setGym(gym1);
-            member5.setGym(gym2);
-            member6.setGym(gym3);
-            member7.setGym(gym1);
-            member8.setGym(gym2);
-            member9.setGym(gym3);
-            member10.setGym(gym1);
 
         }
 
